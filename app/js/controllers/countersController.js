4b70afd9e307bf34c5a8b438caa173c50befef23
Leo Allen <leo.f.allen@gmail.com>
2016-03-08 19:21:06 +0000

    Examples for stage 1

--- app/js/controllers/countersController.js
+++ app/js/controllers/countersController.js
@@ -1,9 +1,37 @@
-countersApp.controller("countersCtrl", function(){
+counterApp.controller('CounterCtrl', function(){
+  // You'll need to explain this
   var self = this;
 
+  // Start with basic two-way data binding
   self.count = 0;
 
+  // Now add buttons
   self.increment = function() {
     self.count++;
-  }
+  };
+
+  // Now get them to build this using a basic app, and add decrement
+  // Now add a second counter
+  // Now add five counters
+  self.decrement = function() {
+    self.count--;
+  };
+});
+
+counterApp.controller('CounterModelCtrl', function(){
+  var self = this;
+
+  self.counters = [{count: 0, increment: function(){}}, {count: 0}];
+
+  // Now add buttons
+  self.increment = function(counter) {
+    counter.count++;
+  };
+
+  // Now get them to build this using a basic app, and add decrement
+  // Now add a second counter
+  // Now add five counters
+  self.decrement = function(counter) {
+    counter.count--;
+  };
 });
