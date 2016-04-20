9b9cd75d029488e4c955e0a7d24fda007a3ac2c0
Paul <paulalexrees@gmail.com>
2016-04-20 10:23:24 +0100

    stage1basics workshop done

--- app/js/controllers/countersController.js
+++ app/js/controllers/countersController.js
@@ -1,9 +1,27 @@
-countersApp.controller("countersCtrl", function(){
+countersApp.controller("countersCtrl", function($interval){
   var self = this;
 
   self.count = 0;
 
-  self.increment = function() {
-    self.count++;
-  }
-});
+  self.myCounters = [
+    {count: 0},
+  ];
+
+  //$interval(function(){self.myCounters[4].count++;}, 1);
+
+  self.addCounter = function() {
+    self.myCounters.push({count:0});
+  };
+
+  self.increment = function(counter) {
+    counter.count++;
+  };
+
+  self.decrement = function(counter) {
+    // $interval(function(){self.count--;},220);
+    counter.count--;
+  };
+
+
+
+ });
