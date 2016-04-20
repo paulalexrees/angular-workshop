8bed91778dcd9e3d87c818e28e65e132634315f1
Paul <paulalexrees@gmail.com>
2016-04-20 11:04:09 +0100

    stage 3 factories services done

--- app/js/controllers/counterController.js
+++ app/js/controllers/counterController.js
@@ -26,20 +26,38 @@ counterApp.controller('ExampleCtrl', function(ExampleFactory, ExampleService){
   self.exampleService = ExampleService;
 });
 
-counterApp.controller('CounterCtrl', function(){
+counterApp.service('CounterService', function(){
   var self = this;
 
-  self.counters = [{count: 0}, {count: 0}];
+  this.getCounters = function() {
+    return [{count: 0}, {count: 0}];
+  };
 
-  // Now add buttons
-  self.increment = function(counter) {
+  this.counters = [{count: 0}, {count: 0}];
+
+  this.increment = function(counter){
     counter.count++;
   };
+
+  this.decrement = function(counter) {
+    counter.count--;
+  };
+
+});
+
+counterApp.controller('CounterCtrl', function(CounterService){
+  var self = this;
+
+  self.counterService = CounterService;
+  // Now add buttons
+  // self.increment = function(counter) {
+  //   counter.count++;
+  // };
 
   // Now get them to build this using a basic app, and add decrement
   // Now add a second counter
   // Now add five counters
-  self.decrement = function(counter) {
-    counter.count--;
-  };
+  // self.decrement = function(counter) {
+  //   counter.count--;
+  // };
 });
